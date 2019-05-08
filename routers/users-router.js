70e74bd96a926b65f1b58a9fd6b714d19cdf209b
Yeshi <tyeshi777@yahoo.com>
2019-05-08 15:45:13 -0700

    almost there

---
+++ routers/users-router.js
@@ -0,0 +1,50 @@
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const Users = require("./users-model.js");
+const jwt = require("jsonwebtoken");
+const secrets = require("../config/secrets.js");
+
+router.post("/register", (req, res) => {
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 8);
+  user.password = hash;
+
+  Users.add(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+router.post("/login", (req, res) => {
+  let { username, password } = req.body;
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+        res.status(200).json({
+          message: `welcome ${username}, ${token}`
+        });
+      } else {
+        res.status(401).json({ message: "invalid credentials" });
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username,
+    roles: ["users"]
+  };
+  const options = {
+    expiresIn: "2hr"
+  };
+  return jwt.sign(payload, secrets.jwt.secret, options);
+}
+module.exports = router;
--- server.js
+++ server.js
@@ -2,12 +2,35 @@ const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
 const morgan = require("morgan");
+const userRouter = require("./routers/users-router.js");
+const session = require("express-session");
+const knexSessionStore = require("connect-session-knex")(session);
 
 const server = express();
+
+const sessionConfig = {
+  name: "monster",
+  secret: "keep it secret, keep it safe",
+  cookie: {
+    httpOnly: true,
+    maxAge: 1000 * 60 * 25,
+    secure: false
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new knexSessionStore({
+    knex: require("./data/dbConfig.js"),
+    createtable: true,
+    clearInterval: 1000 * 60 * 60
+  })
+};
 
 server.use(express.json());
 server.use(helmet());
 server.use(morgan());
 server.use(cors());
+server.use(session(sessionConfig));
+
+server.use("/api", userRouter);
 
 module.exports = server;
